bc5efc91f425953f99933c35312c5a988171bf31
Mahendrashahh <shahmahendra380@gmail.com>
2025-09-18 09:13:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const nextbtn=document.querySelector('.next-btn');
+const video= document.querySelector('.hero-video');
+
+const movielist=['vedio/hero-1.mp4','vedio/hero-2.mp4','vedio/hero-3.mp4','vedio/hero-4.mp4'];
+
+let index =0;
+nextbtn.addEventListener('click',function(){
+
+    index +=1
+    video.src =movielist[index];
+
+    if(index===3){
+        index =-1;
+    }
+
+})
+
+
+const imgBox = document.querySelector('.image-box'); 
+const img = document.querySelector('.image-box img');
+
+window.addEventListener('scroll', function () {
+  
+  imgBox.style.width = window.innerWidth + 'px';
+  imgBox.style.height = window.innerHeight + 'px';
+});
+
+
